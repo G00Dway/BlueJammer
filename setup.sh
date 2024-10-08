33a5dab47a21d64afaf6790de66d05cf000498b3
Marcus Walker <80381071+G00Dway@users.noreply.github.com>
2024-10-08 23:45:50 +0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+cat core/graffiti/banner.txt
+echo "[i] Installing BlueJammer to System (And Requirements)..."
+
+apt update
+apt install bluez bluez-tools bluetooth gnome-bluetooth bluemon blueman -y
+apt install xterm zenity ssh -y
+pip3 install --upgrade pybluez pwntools --break-system-packages
+
+clear
+cat core/graffiti/banner.txt
+echo "[i] Copying files..."
+
+git clone https://github.com/G00Dway/BlueJammer /usr/share/BlueJammerToolkit &> /var/log/bluejammer.log
+cp -r /usr/share/BlueJammerToolkit/core/BlueJammer.sh /usr/bin/bluejammer
+chmod +x /usr/bin/bluejammer
+
+clear
+cat core/graffiti/banner.txt
+echo "[+] Installation Finished!"
+echo '[i] Execute "bluejammer" In your Terminal to run the Script!'
